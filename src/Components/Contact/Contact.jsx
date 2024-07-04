e5214314058a5f85add5eeb2ff98719c1e230b69
A1-mamun <1154almamun@gmail.com>
2024-07-04 23:26:21 +0600

    sample contact section added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -8,76 +8,34 @@ import { MdEmail } from "react-icons/md";
 
 const Contact = () => {
   return (
-    <div className="container mx-auto">
-      <h2 className="w-full text-center text-3xl font-bold">Contact Me</h2>
-      <div className="flex justify-between py-10 px-40">
-        <div className="space-y-5 flex-1">
-          <h2 className="w-full text-center text-3xl font-bold">
+    <div className="bg-black">
+      <div className="container mx-auto flex justify-around py-16 ">
+        <div className="space-y-2">
+          <h2 className="w-full text-center text-5xl font-bold">
             Let&apos;s Connect!
           </h2>
+          <h4 className="text-xl ml-10">I&apos;m just a message away.</h4>
+        </div>
+        <div className=" space-y-5">
           <div className="flex items-center gap-3">
             <MdEmail size={30} />
             <h3 className="text-xl">1154almamun@gmail.com</h3>
           </div>
           <div className="flex items-center gap-3">
             <FaPhoneVolume size={30} />
-            <h3 className="text-xl">+880 1745093497</h3>
+            <h3 className="text-xl">+8801745093497</h3>
           </div>
           <div className="flex items-center gap-4">
             <a href="https://www.linkedin.com/in/a1-mamun" target="_blank">
               <FaLinkedin size={30} />
             </a>
-            <a href="https://www.linkedin.com/in/a1-mamun" target="_blank">
+            <a href="https://github.com/A1-mamun" target="_blank">
               <FaSquareGithub size={30} />
             </a>
-            <a href="https://www.linkedin.com/in/a1-mamun" target="_blank">
+            <a href="https://www.facebook.com/A1Mamun178" target="_blank">
               <FaSquareFacebook size={30} />
             </a>
           </div>
-        </div>
-        <div className="flex-1">
-          <h2 className="w-full text-center text-3xl font-bold">
-            Let&apos;s Talk!
-          </h2>
-          <form className="space-y-5 ">
-            <div>
-              <label className="text-sm">Full name</label>
-              <input
-                id="name"
-                type="text"
-                name="user_name"
-                placeholder=""
-                className="w-full p-2 rounded bg-gray-800"
-                required
-              />
-            </div>
-            <div>
-              <label className="text-sm">Email</label>
-              <input
-                id="email"
-                name="user_email"
-                type="email"
-                className="w-full p-2 rounded bg-gray-800"
-                required
-              />
-            </div>
-            <div>
-              <label className="text-sm">Message</label>
-              <textarea
-                name="message"
-                id="message"
-                rows="3"
-                className="w-full p-2 rounded bg-gray-800"
-                required
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              className="w-full p-3 text-sm font-bold tracki uppercase rounded hover:bg-[#c49037] bg-[#dbb878] text-gray-900"
-            >
-              Send Message
-            </button>
-          </form>
         </div>
       </div>
     </div>
@@ -85,3 +43,32 @@ const Contact = () => {
 };
 
 export default Contact;
+
+{
+  /* <div className="w-full bg-gray-200">
+  <div className="space-y-5 flex flex-col items-center">
+    <h2 className="w-full text-center text-3xl font-bold">
+      Let&apos;s Connect!
+    </h2>
+    <div className="flex items-center gap-3">
+      <MdEmail size={30} />
+      <h3 className="text-xl">1154almamun@gmail.com</h3>
+    </div>
+    <div className="flex items-center gap-3">
+      <FaPhoneVolume size={30} />
+      <h3 className="text-xl">+880 1745093497</h3>
+    </div>
+    <div className="flex items-center gap-4">
+      <a href="https://www.linkedin.com/in/a1-mamun" target="_blank">
+        <FaLinkedin size={30} />
+      </a>
+      <a href="https://github.com/A1-mamun" target="_blank">
+        <FaSquareGithub size={30} />
+      </a>
+      <a href="https://www.facebook.com/A1Mamun178" target="_blank">
+        <FaSquareFacebook size={30} />
+      </a>
+    </div>
+  </div>
+</div>; */
+}
--- src/Layoutes/Main.jsx
+++ src/Layoutes/Main.jsx
@@ -2,6 +2,7 @@ import About from "../Components/About/About";
 import Banner from "../Components/Banner/Banner";
 import Contact from "../Components/Contact/Contact";
 import Footer from "../Components/Footer/Footer";
+import Hire from "../Components/Hire/Hire";
 import Navbar from "../Components/Navbar/Navbar";
 
 const Main = () => {
@@ -10,6 +11,7 @@ const Main = () => {
       <Navbar />
       <Banner />
       <About />
+      <Hire />
       <Contact />
       <Footer />
     </div>
