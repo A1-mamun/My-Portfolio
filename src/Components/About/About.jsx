388ec93443f50bbf290e903f4a6a5f0302928d76
A1-mamun <1154almamun@gmail.com>
2024-07-04 17:00:37 +0600

    aboute section added

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,18 +1,63 @@
+import Lottie from "lottie-react";
+import aboutlottie from "../../assets/LottieFile/about.json";
 const About = () => {
   return (
     <div className="container mx-auto h-[calc(100vh-96px)]">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel similique
-      recusandae eligendi eum magnam esse officia quibusdam exercitationem
-      temporibus? Natus asperiores vel similique commodi laboriosam illum
-      voluptatem quibusdam delectus. Nihil, ipsa assumenda sunt cum nemo,
-      mollitia deserunt enim unde a praesentium deleniti neque atque culpa
-      tempore maxime officia, consequatur explicabo vero! Nesciunt ea est nulla
-      illo, optio provident veritatis voluptas aliquid quidem facere sapiente
-      possimus iure unde voluptatem repellendus et. Fugiat quia est delectus
-      exercitationem eligendi, laudantium incidunt, dignissimos blanditiis
-      porro, minima nisi neque. Voluptatem harum accusamus aperiam velit
-      quibusdam, molestias doloribus placeat, quo ut commodi atque ea, deleniti
-      officia.
+      <div className="hero min-h-screen text-white">
+        <div className="hero-content w-full flex-col lg:gap-20 lg:flex-row justify-between">
+          <div className="flex-1 w-96 ">
+            <Lottie animationData={aboutlottie} className="-ml-10"></Lottie>
+          </div>
+          <div className="flex-1 px-5 lg:px-0">
+            <h1 className="text-5xl font-bold uppercase ">About Me</h1>
+            <p className="py-6">
+              I am a front-end web developer currently studying at Rajshahi
+              University of Engineering and Technology (RUET) in Rajshahi,
+              Bangladesh. I am in my 3rd year in the Department of Computer
+              Science and Engineering. I work with the MERN stack environment
+              and primarily use the ReactJS framework to build dynamic and
+              responsive web applications. Additionally, I am a competitive
+              programmer with some knowledge of C and C++ programming languages.
+              My passion for coding drives me to continuously improve my skills
+              and stay updated with the latest technologies in web development.
+            </p>
+            <div className="space-y-2">
+              <h2 className="font-bold text-lg uppercase">
+                <span className="uppercase ">Name: </span>
+                Md. Abdullah al mamun
+              </h2>
+              <h2 className="font-bold text-lg uppercase">
+                <span className="uppercase ">NATIONALITY: </span>
+                Bangladeshi
+              </h2>
+              <h2 className="font-bold text-lg uppercase">
+                <span className="uppercase ">ADDRESS: </span>
+                Rajshahi, bangladesh
+              </h2>
+              <h2 className="font-bold text-lg uppercase">
+                <span className="uppercase ">PHONE: </span>+880 1745093497
+              </h2>
+              <h2 className="font-bold text-lg ">
+                <span className="uppercase ">E-MAIL: </span>
+                1154almamun@gmail.com
+              </h2>
+              <h2 className="font-bold text-lg uppercase">
+                <span className="uppercase ">Languages: </span>
+                English,Bangla,Hindi
+              </h2>
+              <h2 className="font-bold text-lg uppercase">
+                <span className="uppercase ">Education: </span>
+                computer science & engineering <br /> (3rd year)
+              </h2>
+              <h2 className="font-bold text-lg uppercase">
+                <span className="uppercase ">University: </span>
+                Rajshahi University of Engineering and Technology (RUET)
+                Rajshahi, Bangladesh
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
